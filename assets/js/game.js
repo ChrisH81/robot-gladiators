f73f5348e24f19e638b0968c4ac45e5370c34a5c
Chris H <kekaneone@gmail.com>
2020-05-12 16:36:30 -0600

    finished first code initial

--- assets/js/game.js
+++ assets/js/game.js
@@ -44,11 +44,21 @@ var fight = function () {
       window.alert(playerName + " still has " + playerHealth + " health left.");
     }
     // if player choses to skip
-
   } else if (promptFight === "skip" || promptFight === "SKIP") {
-    window.alert(playerName + " has chosen to skip the fight!");
-  } else {
-    window.alert("You need to pick a valid option. Try again!");
+
+  // confirm user wants to skip
+  var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+  // if yes (true), leave fight
+  if (confirmSkip) {
+    window.alert(playerName + " has decided to skip this fight. Goodbye!");
+    // subtract money from playerMoney for skipping
+    playerMoney = playerMoney - 2;
   }
+  // if no (false), ask question again by running fight() again
+  else {
+    fight();
+  }
+}
 };
 fight();
