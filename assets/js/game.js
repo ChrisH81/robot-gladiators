9c16eca1219c3aee72a74d64f096a2813c509ddb
Chris H <kekaneone@gmail.com>
2020-05-12 18:17:12 -0600

    Saving multi fight bot without luck not working

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,6 +4,14 @@ var playerAttack = 40;
 var playerMoney = 10;
 
 
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+for(var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
+
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
@@ -11,7 +19,9 @@ var enemyName = "Roborto";
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function () {
+var fight = function(enemyName) {
+  // fight function statements
+
   // Alert users that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
   var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -57,8 +67,8 @@ var fight = function () {
   }
   // if no (false), ask question again by running fight() again
   else {
-    fight();
+   fight();
   }
 }
-};
+}
 fight();
