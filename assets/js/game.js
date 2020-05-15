b876c116592b3a6421b2b61cd0ae489fcca3896f
Chris H <kekaneone@gmail.com>
2020-05-15 16:25:15 -0600

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,12 +9,47 @@ var enemyAttack = 12;
 
 var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
-for (var i = 0; i < enemyNames.length; i = i + 1) {
+var startGame = function () {
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
+  for (var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0) {
+      window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
 
-  enemyHealth = 50;
-  fight(enemyNames[i]);
+      var pickedEnemyName = enemyNames[i];
+
+      enemyHealth = 50;
+
+      fight(pickedEnemyName);
+    }
+    else {
+      window.alert("You have lost your robot in battle! Game Over!");
+      break;
+    }
+  }
+  endGame();
+};
+startGame();
+function endGame() {
+  // if player is still alive, player wins!
+  if (playerHealth > 0) {
+    window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+  }
+  else {
+    window.alert("You've lost your robot in battle.");
+  }
+  // ask player if they'd like to play again
+  var playAgainConfirm = window.confirm("Would you like to play again?");
+
+  if (playAgainConfirm) {
+    // restart the game
+    startGame();
+  }
+  else {
+    window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+  }
 }
-
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
