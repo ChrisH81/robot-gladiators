410187ce574d1a9707ec7244e65d359d0ae014c5
Chris H <kekaneone@gmail.com>
2020-05-16 16:15:41 -0600

    fight skip functionality

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,8 +3,18 @@ var randomNumber = function(min, max) {
 
   return value;
 };
+// function to set name
+var getPlayerName = function() {
+  var name = "";
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+
+  console.log("Your robot's name is " + name);
+  return name;
+};
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
@@ -132,11 +142,41 @@ function endGame() {
 // You can also log multiple values at once like this
 console.log(playerInfo.name, playerInfo.attack, playerInfo.health);
 
-function fight(enemy) {
-  while (enemy.health > 0 && playerInfo.health > 0) {
-    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+var fightOrSkip = function() {
+  // ask user if they'd like to fight or skip using  function
+  var promptFight = window.prompt('Would you like FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+  promptFight = promptFight.toLowerCase();
+  // Conditional Recursive Function Call
+if (promptFight === "" || promptFight === null) {
+  window.alert("You need to provide a valid answer! Please try again.");
+  return fightOrSkip();
+}
 
-    if (promptFight === "skip" || promptFight === "SKIP") {
+
+  // if user picks "skip" confirm and then stop the loop
+  if (promptFight === "skip") {
+    // confirm user wants to skip
+    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+    // if yes (true), leave fight
+    if (confirmSkip) {
+      window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
+      // subtract money from playerMoney for skipping
+      playerInfo.playerMoney = playerInfo.money - 10;
+      return true;
+    }
+  }
+  return false;
+}
+
+function fight(enemy) {
+  // repeat and execute as long as the enemy robot is alive 
+  while(enemy.health > 0 && playerInfo.health > 0) {
+      if (fightOrSkip()) {
+        // if true, leave fight by breaking loop
+        break;
+      }
+      var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
 
       // confirm user wants to skip
       var confirmSkip = window.confirm("Are you sure you'd like to quit?");
